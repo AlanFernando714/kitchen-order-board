acedfb4607b237af27878081619ec0b04c45b085
Alan Carlos <alan123.afcf@gmail.com>
2025-10-10 12:56:38 -0600

    __main__.py modificacion en archivo main, imports relativos

--- kitchen_orders/__main__.py
+++ kitchen_orders/__main__.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from .data.storage import load_orders
 from .ui.main_window import MainWindow
 
+#Funcion para ejecutar directamente desde run.py
 def main():
     orders = load_orders()
     root = tk.Tk()
