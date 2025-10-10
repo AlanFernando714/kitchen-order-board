bd6862cf85e75f6c090166a28e9fa8aa055a3885
Alan Carlos <alan123.afcf@gmail.com>
2025-10-10 12:51:45 -0600

    Archivo para crear un ejecutable run.py

---
+++ kitchen_orders/__main__.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from .data.storage import load_orders
+from .ui.main_window import MainWindow
+
+def main():
+    orders = load_orders()
+    root = tk.Tk()
+    app = MainWindow(root, orders)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ run.py
@@ -0,0 +1,4 @@
+from kitchen_orders.__main__ import main
+
+if __name__ == "__main__":
+    main()
